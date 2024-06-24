4bfb0685f74a8862d30db01af8500a7637c81979
Your Name <you@example.com>
2024-06-24 09:39:11 +0300

    Message_Success

--- Sepio-App/front-end/src/components/SignUp.js
+++ Sepio-App/front-end/src/components/SignUp.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import FormControl from '@mui/joy/FormControl';
 import FormLabel from '@mui/joy/FormLabel';
 import Input from '@mui/joy/Input';
 import Button from '@mui/joy/Button';
 import SepioLogo from './../image/Sepio_Logo.png';
+import {Toast} from 'primereact/toast';
 import axios from 'axios';
 
 export default function SignUp() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ username: '', password: '' });
   const [status, setStatus] = useState('initial');
+  const toast = useRef(null);
+
+  const showSuccess = (message) => {
+    toast.current.show({severity: 'success', summary: 'Success', detail: message, life: 3000});
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setStatus('loading');
+
+
     axios.post('/signup', formData)
       .then(response => {
         if (response.data.success) {
+          showSuccess('User created successfully');
+          setTimeout(() => {
           navigate('/login');
+          }, 1500)
+          
         } else {
           setStatus('failure');
         }
@@ -34,6 +47,7 @@ export default function SignUp() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', background: '#778899', padding: '40px', borderRadius: '10px', maxWidth: '400px', margin: 'auto', marginTop: '100px' }}>
+    <Toast ref = {toast}/>
       <img src={SepioLogo} alt="Welcome" style={{ marginBottom: '20px', height: 70 }} />
       <form onSubmit={handleSubmit}>
         <FormControl>
@@ -54,6 +68,7 @@ export default function SignUp() {
             onChange={(e) => setFormData({ ...formData, password: e.target.value })}
           />
           <Button
+            loading = {status === 'loading'}
             variant="solid"
             color="primary"
             type="submit"
